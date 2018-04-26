714aad57db176e5853a5575ee5cc6b7a558a2fdb
Matt Barraza-Carl <mccarl@asu.edu>
2018-04-26 00:09:57 -0700

    prettier

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
-import React, {Component} from 'react'
-import {StyleSheet, Text, TouchableHighlight, View} from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 export default class App extends Component {
-  state = {backgroundColor: '#ffffff'}
+  state = { backgroundColor: '#ffffff' };
 
   onTouchableHighlightPress = () => {
-    this.setState({backgroundColor: randomHex()})
-  }
+    this.setState({ backgroundColor: randomHex() });
+  };
 
   render() {
     return (
       <View
         style={[
           styles.container,
-          {backgroundColor: this.state.backgroundColor}
-        ]}>
+          { backgroundColor: this.state.backgroundColor },
+        ]}
+      >
         <TouchableHighlight
           style={styles.touchableHighlight}
-          onPress={this.onTouchableHighlightPress}>
+          onPress={this.onTouchableHighlightPress}
+        >
           <View style={styles.button}>
             <Text style={styles.buttonText}>Tap Me</Text>
           </View>
         </TouchableHighlight>
       </View>
-    )
+    );
   }
 }
 
@@ -31,14 +33,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 
   touchableHighlight: {
     padding: 1,
     borderRadius: 50,
     borderColor: '#ffffff',
-    borderWidth: 1
+    borderWidth: 1,
   },
 
   button: {
@@ -47,24 +49,24 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000000'
+    backgroundColor: '#000000',
   },
 
   buttonText: {
     color: '#ffffff',
     fontWeight: 'bold',
-    fontSize: 24
-  }
-})
+    fontSize: 24,
+  },
+});
 
 function randomUnit(chars = '0123456789ABCDEF') {
-  return chars[Math.floor(Math.random() * 16)]
+  return chars[Math.floor(Math.random() * 16)];
 }
 
 function randomHex(color = '#', i = 0) {
   while (i < 6) {
-    color += randomUnit()
-    i++
+    color += randomUnit();
+    i++;
   }
-  return color
+  return color;
 }
